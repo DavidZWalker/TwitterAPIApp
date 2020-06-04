b7eff8bb6636a2124edb502a480b798f2d17fb37
David <dw068@hdm-stuttgart.de>
2020-06-04 15:04:40 +0200

    add project files

---
+++ TwitterAPIApp/TwitterAPIApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TwitterAPIApp
+//
+//  Created by David Zack Walker on 04.06.20.
+//  Copyright © 2020 dsfw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
