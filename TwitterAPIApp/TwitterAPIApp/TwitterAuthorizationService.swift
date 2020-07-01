678813f74edab5f49fd410009e08dd0d7b91f470
David <dw068@hdm-stuttgart.de>
2020-07-01 18:20:22 +0200

    fix merge conflicts

---
+++ TwitterAPIApp/TwitterAPIApp/TwitterAuthorizationService.swift
@@ -0,0 +1,13 @@
+//
+//  TwitterAuthorizationService.swift
+//  TwitterAPIApp
+//
+//  Created by David Zack Walker on 01.07.20.
+//  Copyright © 2020 dsfw. All rights reserved.
+//
+
+import Foundation
+
+class TwitterAuthorizationService {
+    
+}
