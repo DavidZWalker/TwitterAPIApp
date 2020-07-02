c599d395e0bc903ec753110de4c78c4014af563a
Schneider Fabian <fs126@hdm-stuttgart.de>
2020-07-02 15:56:36 +0200

    Connected StoryBoard with Tweet class

--- TwitterAPIApp/TwitterAPIApp/TweetsTableViewController.swift
+++ TwitterAPIApp/TwitterAPIApp/TweetsTableViewController.swift
@@ -7,10 +7,24 @@
 //
 
 import UIKit
+class TweetTableViewCell: UITableViewCell{
+    
+    @IBOutlet weak var tweetProfilePicture: UIImageView!
+    @IBOutlet weak var tweetNickname: UILabel!
+    @IBOutlet weak var tweetUsername: UILabel!
+    @IBOutlet weak var tweetText: UILabel!
+    @IBOutlet weak var tweetCommentCount: UILabel!
+    @IBOutlet weak var tweetRetweetCount: UILabel!
+    @IBOutlet weak var tweetLikeCount: UILabel!
+    @IBOutlet weak var tweetLocation: UILabel!
+    
+    
+    
+}
 
 class TweetsTableViewController: UITableViewController {
     
-    //var tweets = [Tweet]()
+    var tweets = [Tweet]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,17 +36,23 @@ class TweetsTableViewController: UITableViewController {
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-    /*override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-     return tweets.count
-     }
-     
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tweets.count
+    }
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "tweetCell", for: indexPath)
-        
-        cell.textLabel?.text = items[indexPath.row].Tweet
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tweetCell", for: indexPath) as! TweetTableViewCell
+        //cell.tweetProfilePicture = tweets[indexPath.row]
+        cell.tweetNickname.text = tweets[indexPath.row].author
+        //cell.tweetUsername.text = tweets[indexPath.row].author
+        cell.tweetText.text = tweets[indexPath.row].content
+        cell.tweetCommentCount.text = String(tweets[indexPath.row].replies.count)
+        cell.tweetRetweetCount.text = String(tweets[indexPath.row].retweetCount)
+        cell.tweetLikeCount.text = String(tweets[indexPath.row].likeCount)
+        //cell.tweetLocation.text = String(tweets[indexPath.row].location)
         
         return cell
     }
- */
+    
     
 }
